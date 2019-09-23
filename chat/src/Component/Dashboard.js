729e52a45b707e016a3bf93035a3bc1fa4fcd4aa
lchoo <lchoo1996@gmail.com>
2019-09-22 23:52:32 -0700

    refactor: added dynamic widths for chat

--- chat/src/Component/Dashboard.js
+++ chat/src/Component/Dashboard.js
@@ -32,8 +32,16 @@ const Dashboard = () => {
         chatWindow: {
             width: "70%",
             borderLeft: "1px inset grey",
-            height: "500px",
+            height: "400px",
             overflowY: "auto",
+            // overflowX: "hidden",
+        },
+        chatLeft: {
+            width: "30%",
+        },
+        chatTopic: {
+            margin: "1rem 0",
+            width: "70%",
         },
         chatBox: {
             width: "85%",
@@ -41,7 +49,7 @@ const Dashboard = () => {
         button: { width: "15%" },
         messengerLeft: {
             marginLeft: "30%",
-            width: "40%",
+            width: "60%",
             borderRadius: "5px",
         },
         messengerRight: { width: "auto" },
@@ -57,9 +65,9 @@ const Dashboard = () => {
     // local state
     const [activeTopic, changeActiveTopic] = useState(topics[0]);
     const [textValue, changeTextValue] = useState("");
-    const [color, changeColor] = useState(false);
     const [currentIndex, changeCurrentIndex] = useState(0);
 
+    //if press enter, check if there text if there is then emit the message
     const handleKeyDown = e => {
         if (e.key === "Enter") {
             if (e.target.value !== "") {
@@ -88,9 +96,18 @@ const Dashboard = () => {
                     >
                         Let's Chat
                     </Typography>
-                    <Typography variant="h5" component="h5">
-                        {activeTopic}
-                    </Typography>
+                    <div className={classes.flex}>
+                        <div className={classes.chatLeft}></div>
+                        <div className={classes.chatTopic}>
+                            <Typography
+                                variant="h5"
+                                component="h5"
+                                style={{ fontWeight: "bolder" }}
+                            >
+                                {activeTopic}
+                            </Typography>
+                        </div>
+                    </div>
                     <div className={classes.flex}>
                         <div className={classes.topicsWindow}>
                             <List>
@@ -112,18 +129,61 @@ const Dashboard = () => {
                         </div>
 
                         <div className={`${classes.chatWindow} `}>
+                            <div
+                                style={{
+                                    textAlign: "left",
+                                    paddingLeft: "2rem",
+                                    paddingBottom: "1rem",
+                                }}
+                            >
+                                Welcome to the chatroom
+                            </div>
                             {allChats[activeTopic].map((chat, index) => (
-                                <div className="talk-bubble round " key={index}>
-                                    <div className="chatItems chatItems__user">
-                                        {chat.from}
-                                    </div>
+                                <>
+                                    {chat.msg.length > 50 ? (
+                                        <div
+                                            className="talk-bubble round "
+                                            key={index}
+                                            style={{
+                                                width: "90%",
+                                            }}
+                                        >
+                                            <div className="chatItems chatItems__user">
+                                                {chat.from}
+                                            </div>
 
-                                    <div className="chatItems">
-                                        <Typography variant="body1">
-                                            {chat.msg}
-                                        </Typography>
-                                    </div>
-                                </div>
+                                            <div
+                                                className="chatItems"
+                                                style={{ maxWidth: "100%" }}
+                                            >
+                                                <Typography variant="body1">
+                                                    {chat.msg}
+                                                </Typography>
+                                            </div>
+                                        </div>
+                                    ) : (
+                                        <div
+                                            className="talk-bubble round "
+                                            key={index}
+                                            style={{
+                                                width: "auto",
+                                            }}
+                                        >
+                                            <div className="chatItems chatItems__user">
+                                                {chat.from}
+                                            </div>
+
+                                            <div
+                                                className="chatItems"
+                                                style={{ maxWidth: "100%" }}
+                                            >
+                                                <Typography variant="body1">
+                                                    {chat.msg}
+                                                </Typography>
+                                            </div>
+                                        </div>
+                                    )}
+                                </>
                             ))}
                         </div>
                     </div>
