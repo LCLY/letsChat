e2f3880921c0ff4d8c627b33b46595c0db5b30a4
lchoo <lchoo1996@gmail.com>
2019-09-23 05:17:15 -0700

    refactor: added input component

--- chat/src/Component/Input.js
+++ chat/src/Component/Input.js
@@ -1,4 +1,31 @@
-import React from "react";
-const Input = () => {};
+import React, { useState } from "react";
+import { TextField, makeStyles } from "@material-ui/core";
+import { CTX } from "../Store";
+const Input = () => {
+    //CTX Store
+    const { newUsername } = React.useContext(CTX);
+
+    const useStyles = makeStyles(theme => ({
+        textField: {
+            marginLeft: theme.spacing(1),
+            marginRight: theme.spacing(1),
+        },
+    }));
+    const classes = useStyles();
+    const [username, changeUsername] = useState("");
+    return (
+        <TextField
+            label="Username"
+            className={classes.textField}
+            value={username}
+            onChange={e => {
+                changeUsername(e.target.value);
+            }}
+            onBlur={newUsername(username)}
+            margin="normal"
+            variant="outlined"
+        />
+    );
+};
 
 export default Input;
