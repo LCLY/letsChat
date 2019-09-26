0d80d15e542dbff6c98931ae2d054bd93b42cefe
lchoo <lchoo1996@gmail.com>
2019-09-26 01:29:15 -0700

    build: built files for static server

---
+++ client/build/precache-manifest.5dd9c3bd06b9a9d825fe31966b5b2309.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c669aff983048e3ac49b7170a3888f4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c647a48dd7211ef74f83",
+    "url": "/static/css/main.172edd93.chunk.css"
+  },
+  {
+    "revision": "d5c2956ce37000f5a472",
+    "url": "/static/js/2.8acb8d36.chunk.js"
+  },
+  {
+    "revision": "c647a48dd7211ef74f83",
+    "url": "/static/js/main.d05295e9.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  }
+]);
